cc426582264048550f827ee11ffeb8c55b4d3fbc
Bidipta-BG <gogoibidipta@gmail.com>
2022-06-03 13:05:55 +0530

    updated with pritesh sir assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -105,4 +105,44 @@ router.get('/films/:filmId', function (req, res) {
 
 
 
+
+//Pritesh sir assignment
+
+//1.API to find missing number in the array [1,2,3,5,6,7]
+router.get("/sol1", function (req, res) {
+   let arr= [1,2,3,5,6,7]
+ 
+   let total = 0;
+   for (var i in arr) {
+       total += arr[i];
+   }
+ 
+   let lastDigit= arr.pop()
+   let consecutiveSum= lastDigit * (lastDigit+1) / 2
+   let missingNumber= consecutiveSum - total
+ 
+   res.send('The missing number is: '+ missingNumber);
+ });
+
+
+//2.API to find missing number in the array
+router.get("/sol2", function (req, res) {
+   let arr= [33, 34, 35, 37, 38]
+   let len= arr.length
+ 
+   let total = 0;
+   for (var i in arr) {
+       total += arr[i];
+   }
+ 
+   let firstDigit= arr[0]
+   let lastDigit= arr.pop()
+   let consecutiveSum= (len + 1) * (firstDigit+ lastDigit ) / 2
+   let missingNumber= consecutiveSum - total
+  
+   res.send('The missing number is: '+ missingNumber);
+ });
+
+
+
 module.exports = router;
