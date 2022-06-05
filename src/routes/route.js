01aaac926af07aa573ca4285e41590f3448534d0
Bidipta-BG <gogoibidipta@gmail.com>
2022-06-05 13:35:12 +0530

    mongo1stassignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,14 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const UserModel= require("../models/userModel.js")
 const UserController= require("../controllers/userController")
 
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
+router.post("/createbook", UserController.createBook)
 
-router.post("/createUser", UserController.createUser  )
-
-router.get("/getUsersData", UserController.getUsersData)
+router.get("/getbook", UserController.getAllBooks)
 
 module.exports = router;
