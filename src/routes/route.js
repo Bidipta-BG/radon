ae94b33fe2f8b4217e76ba35ab5e72d17c0f7a0b
Bidipta-BG <gogoibidipta@gmail.com>
2022-06-07 13:20:30 +0530

    mongo_assignment_2

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const UserController= require("../controllers/userController")
+const UserController= require("../controllers/bookController")
 
 router.post("/createbook", UserController.createBook)
 
-router.get("/getbook", UserController.getAllBooks)
+router.get("/booklist", UserController.bookList)
+
+router.post("/booksbyyear", UserController.getBooksYear)
+
+router.post("/getparticularbook", UserController.getParticularBooks)
+
+router.get("/booksinr", UserController.getXINRBooks)
+
+router.get("/randbooks", UserController.getRandomBooks)
 
 module.exports = router;
